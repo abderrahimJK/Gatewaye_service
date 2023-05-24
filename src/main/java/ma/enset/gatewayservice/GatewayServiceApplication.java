f9402313ed0b6f19e9cd814b60a026f41c6741b8
abderrahimJK <abderrahim.aitbouna@gmail.com>
2023-05-24 16:07:57 +0000

    gateway service

---
+++ src/main/java/ma/enset/gatewayservice/GatewayServiceApplication.java
@@ -0,0 +1,35 @@
+package ma.enset.gatewayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
+import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
+import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
+
+@SpringBootApplication
+public class GatewayServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayServiceApplication.class, args);
+    }
+
+    @Bean
+    DiscoveryClientRouteDefinitionLocator definitionLocator(ReactiveDiscoveryClient rdc, DiscoveryLocatorProperties properties){
+        return new DiscoveryClientRouteDefinitionLocator(rdc, properties);
+    }
+
+//    RouteLocator routeLocator(RouteLocatorBuilder builder){
+//        return builder.routes()
+//                .route((r)-> r.path("/proprietaires/**").uri("http://localhost:5051/")).build();
+//    }
+
+
+}
